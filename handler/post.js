525164b3e805dc40a1dd02c1e1dc8ee94bebf39a
maccyber <jonas.enge@gmail.com>
2017-03-07 15:18:23 +0100

    first commit

--- handler/post.js
+++ handler/post.js
@@ -4,10 +4,9 @@ const bodyParser = require('urlencoded-body-parser')
 const createTicket = require('../lib/create-ticket')
 const postForm = require('../lib/post-form')
 
-module.exports = async (request) => {
+module.exports = async request => {
   const data = await bodyParser(request)
-  const ticket = await createTicket(data)
-  const caseNumber = await postForm(ticket)
-  console.log(caseNumber)
-  return { id: caseNumber.data }
+  const ticket = createTicket(data)
+  const result = await postForm(ticket)
+  return result
 }
